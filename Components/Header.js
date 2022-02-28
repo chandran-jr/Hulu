869b3e28bff9032b5eed998e90d0174ce95b69db
Govind B Chandran <govindchandran235@gmail.com>
2022-02-28 20:52:16 +0530

    Add files via upload

--- Components/Header.js
+++ Components/Header.js
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 function Header() {
   return (
     <header className="">
         <h1>This is the header</h1>
+        <Image src="https://links.papareact.com/ua6" width={200} height={100} />
     </header>
   )
 }
