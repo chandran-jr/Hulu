4f876c03da51181814d0ffc6e361a9c6968aa158
Govind B Chandran <govindchandran235@gmail.com>
2022-02-28 20:59:11 +0530

    Add files via upload

--- Components/Header.js
+++ Components/Header.js
@@ -4,7 +4,11 @@ function Header() {
   return (
     <header className="">
         <h1>This is the header</h1>
-        <Image src="https://links.papareact.com/ua6" width={200} height={100} />
+        <Image 
+        className="object-contain" 
+        src="https://links.papareact.com/ua6" 
+        width={200} 
+        height={100} />
     </header>
   )
 }
