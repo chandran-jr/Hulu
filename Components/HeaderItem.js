3a5ea2210d2a83c4f2f47299826c8b609764161f
Govind B Chandran <govindchandran235@gmail.com>
2022-02-28 21:31:31 +0530

    Add files via upload

--- Components/HeaderItem.js
+++ Components/HeaderItem.js
@@ -1,7 +1,7 @@
 function HeaderItem({title,Icon}) {
   return (
-    <div className="group">
-        <Icon className="h-8 mb-1"/>
+    <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 group hover:text-white">
+        <Icon className="h-8 mb-1 group hover:animate-bounce"/>
         <p className=" opacity-0 tracking-widest group-hover:opacity-100">{title}</p>
     </div>
   )
