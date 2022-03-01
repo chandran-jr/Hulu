8db53aa2b63ca61e43f6d106b0992f74ad86be82
Govind B Chandran <govindchandran235@gmail.com>
2022-03-01 22:01:50 +0530

    Add files via upload

--- Components/Nav.js
+++ Components/Nav.js
@@ -3,9 +3,9 @@ import requests from "../Utils/requests";
 function Nav() {
   return (
       <nav>
-         <div className=''>
+         <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll overflow-y-hidden'>
             {Object.entries(requests).map(([key, {title,url}]) =>(
-                <h2 key={key}>{title}</h2>
+                <h2 key={key} className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500">{title}</h2>
             ))}
         </div>
       </nav>
