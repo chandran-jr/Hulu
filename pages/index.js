1d4aee645f76cb9cf9c02b0c884cd949efcb2f9e
Govind B Chandran <govindchandran235@gmail.com>
2022-03-02 10:47:14 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../Components/Header'
 import Nav from '../Components/Nav'
+import Results from '../Components/Results'
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
 
       <Nav/>
 
-      
+      <Results/>
     </div>
   )
 }
