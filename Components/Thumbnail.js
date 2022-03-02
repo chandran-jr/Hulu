f96a9bd14fb506be4fc2d3eab4c0bda8976855f7
Govind B Chandran <govindchandran235@gmail.com>
2022-03-03 01:26:55 +0530

    Add files via upload

--- Components/Thumbnail.js
+++ Components/Thumbnail.js
@@ -1,8 +1,17 @@
+import Image from "next/image"
 
 
-function Thumbnail(props) {
+function Thumbnail({result}) {
+    const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   return (
-    <div>Thumbnail</div>
+    <div>
+        <Image
+            layout="responsive"
+            src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.backdrop_path}`}
+            height={1080}
+            width={1920}
+        />
+    </div>
   )
 }
 
